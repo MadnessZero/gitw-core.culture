01cdeef5bc0dafd400c003013c397511a290f871
Madness Zero <jmasini.soft@gmail.com>
2019-02-14 14:26:32 +0100

    Culture 1.1.0 - Mise en place d'un TargetLocalizer

---
+++ Culture/SharedCulture/TargetLocalizer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace GitWCore.Culture.SharedCulture
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TargetLocalizer<T>
+    {
+        private readonly IStringLocalizer _localizer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public LocalizedString this[string key]
+        {
+            get
+            {
+                return _localizer[key];
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="factory"></param>
+        public TargetLocalizer(IStringLocalizerFactory factory)
+        {
+            var type = typeof(T);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create(type.Name, assemblyName.Name);
+        }
+    }
+}
